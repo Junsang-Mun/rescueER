d74dbdee7b78be486528403aafcf45fc40cbfe70
Junsang-Mun <mun.js@aol.com>
2022-08-06 18:23:07 +0900

    Add Feature; API 조회하는 기능 추가 완료
    - 다만 로컬 머신에서 테스트했을 때  certErr 표출
    - 이건 api 서버 문제 같은데 내가 해결할 순 없을 것 같음

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,101 @@
+import 'dart:convert';
+//import 'dart:html';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+//import 'package:english_words/english_words.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: HomePage(),
+    );
+  }
+}
+
+class ERdata {
+  String erName; // 응급실 이름
+  String updateDate; // 최신화 날짜
+  String availableER; // 가용 응급실 베드 수
+  String availableOR; // 가용 수술실 수
+  String availableRM; // 가용 입원실 수
+  String availableMICU; // 가용 내과중환자실 베드 수
+  String availableSICU; // 가용 외과중환자실 베드 수
+  String availableNSCICU; // 가용 신경외과중환자실 베드 수
+  bool ctYN; // CT 촬영 가용 여부
+  bool agYN; // 조영촬영 가용 여부
+  bool mriYN; // MRI 촬영 가용 여부
+  bool ventYN; //인공호흡기 가용 여부
+
+  ERdata(
+      {this.erName = '0',
+      this.updateDate = '0',
+      this.availableER = '0',
+      this.availableOR = '0',
+      this.availableRM = '0',
+      this.availableMICU = '0',
+      this.availableSICU = '0',
+      this.availableNSCICU = '0',
+      this.ctYN = false,
+      this.agYN = false,
+      this.mriYN = false,
+      this.ventYN = false});
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  late XmlDocument apiData;
+  ERdata erData = ERdata();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('rescueER'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.cloud_sync),
+              onPressed: () async {
+                const url =
+                    'https://apis.data.go.kr/B552657/ErmctInfoInqireService/getEmrrmRltmUsefulSckbdInfoInqire?serviceKey=&STAGE1=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&STAGE2=%EA%B0%95%EB%82%A8%EA%B5%AC&pageNo=1&numOfRows=10';
+                var response = await http.get(Uri.parse(url));
+                apiData = XmlDocument.parse(utf8.decode(response.bodyBytes));
+                final datas = apiData.findAllElements('item');
+                print(datas);
+                datas.forEach((element) {
+                  erData = ERdata(
+                    erName: element.getElement('dutyname')!.text,
+                    updateDate: element.getElement('hvidate')!.text,
+                    availableER: element.getElement('hvec')!.text,
+                    availableOR: element.getElement('hvoc')!.text,
+                    availableRM: element.getElement('hvgc')!.text,
+                    availableMICU: element.getElement('hv2')!.text,
+                    availableSICU: element.getElement('hv3')!.text,
+                    availableNSCICU: element.getElement('hv6')!.text,
+                  );
+                  print(erData.erName);
+                });
+              })
+        ],
+      ),
+    );
+  }
+}
+
+/*
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -115,3 +206,4 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 }
+*/
